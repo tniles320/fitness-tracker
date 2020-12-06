98ad39fa16757cb7852dd17322ea763bc65a42e4
Taylor Niles <tniles320@gmail.com>
2020-12-05 16:50:37 -0800

    local functionality complete, heroku functionality needed

--- models/Workout.js
+++ models/Workout.js
@@ -3,49 +3,57 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  date: {
+  day: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   exercises: [
     {
       type: {
-          type: String,
-          trim: true,
-          required: "Exercise type is required"
+        type: String,
+        trim: true,
+        required: "Exercise type is required",
       },
       name: {
-          type: String,
-          trim: true,
-          required: "Exercise name is required"
+        type: String,
+        trim: true,
+        required: "Exercise name is required",
       },
       distance: {
-          type: Number,
-          trim: true,
-          required: "Total distance required"
+        type: Number,
+        trim: true,
+        required: "Total distance required",
       },
       duration: {
-          type: Number,
-          trim: true,
-          required: "Total duration required"
+        type: Number,
+        trim: true,
+        required: "Total duration required",
       },
       weight: {
-          type: Number,
-          trim: true,
-          required: "Weight lifted required"
+        type: Number,
+        trim: true,
+        required: "Weight lifted required",
       },
       sets: {
-          type: Number,
-          trim: true,
-          required: "Number of sets required"
+        type: Number,
+        trim: true,
+        required: "Number of sets required",
       },
       reps: {
-          type: Number,
-          trim: true,
-          required: "Number of reps required"
-      }
-    }
-  ]
+        type: Number,
+        trim: true,
+        required: "Number of reps required",
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  totalDuration: {
+    type: Number,
+    default: 0,
+  },
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,33 +1,51 @@
 const db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/api/workouts", (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-      res.json(dbWorkout);
-    });
-  });
-
-  app.get("/api/workouts/:id", (req, res) => {
-    db.Workout.findOne({ _id: req.params.id }).then(dbWorkout => {
-      res.json(dbWorkout);
-    });
+    db.Workout.find({})
+      .populate("exercises")
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
 
   app.get("/api/workouts/range", (req, res) => {
-    db.Workout.find({}).then(dbWorkout => {
-      res.json(dbWorkout);
-    });
+    db.Workout.find()
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
 
-  app.post("/api/workouts/:id", ({ body }, res) => {
+  app.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $push: { exercises: req.body },
+        $inc: { totalDuration: req.body.duration },
+      },
+      { new: true }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
 };
-
-
